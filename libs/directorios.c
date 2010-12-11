e024d38b362ff0fa8a2eba4a58c6f12e43500f6d
Vicenç Juan Tomàs <vtomasr5@gmail.com>
2010-12-11 18:45:34 +0100

    added function in directorios.c

--- libs/directorios.c
+++ libs/directorios.c
@@ -28,10 +28,40 @@
 
 #include "directorios.h"
 
-
+/*
+ *  Funció que estreu el camí de la ruta que se li passa com a paràmetre i que
+ *  permet diferenciar entre fitxers i directoris.
+ *  @param cami ruta completa cap a l'arxiu que es vol referenciar
+ *  @param inicial primera part de la ruta compresa entre els dos primers '/'
+ *  @param final la resta de la ruta
+ *  @return tipus el tipus d'arxiu que fa referència la ruta completa
+ */
 int extreureCami(const char *cami, char *inicial, char *final)
 {
-    return 0;
+    int i = 0;
+    int j = 0;
+    int tipus = 1; // tipus directori
+
+    if (cami[i] != '/') {
+        printf("[directorios.c] ERROR: Ruta incorrecta!\n");
+        return -1;
+    }
+
+    i++;
+    while ((cami[i] != '/') && (cami[i] != '\0')) { // obtenim lo que hi ha entre els dos primers '/'
+        inicial[i-1] = cami[i];
+        i++;
+    }
+    inicial[i] = '\0';
+
+    while (cami[i] != '\0') { // obtenim el reste de la ruta
+        final[j] = cami[i];
+        i++;j++;
+        tipus = 0; // tipus fitxer
+    }
+    final[j] = '\0';
+
+    return tipus;
 }
 
 int cercarEntrada(const char *cami_parcial, unsigned int *p_inode_dir, unsigned int *p_inode, unsigned int *p_entrada, char reservar)
